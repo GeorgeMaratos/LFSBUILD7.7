14b887c015968f867d86f82cdab9afc01705e2aa
George <ffmpeg12341@gmail.com>
2015-06-03 20:49:57 +0000

    fixed a syntax highlighting problem with [

--- install_lfs_2.sh
+++ install_lfs_2.sh
@@ -632,7 +632,7 @@ mv -v /usr/bin/chroot /usr/sbin
 mv -v /usr/share/man/man1/chroot.1 /usr/share/man/man8/chroot.8
 sed -i s/\"1\"/\"8\"/1 /usr/share/man/man8/chroot.8
 
-mv -v /usr/bin/{head,sleep,nice,test,[} /bin 
+mv -v /usr/bin/{head,sleep,nice,test,\[} /bin 
 
 cd ..
 
